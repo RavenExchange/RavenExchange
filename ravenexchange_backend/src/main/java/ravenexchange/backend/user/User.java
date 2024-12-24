13b718b1181cf73bea9bca61292e00db4cb17729
Haashir Tanveer <haashirtanveer@gmail.com>
2024-12-23 21:23:27 -0500

    register endpoint functional (api/auth/register)

--- ravenexchange_backend/src/main/java/ravenexchange/backend/user/User.java
+++ ravenexchange_backend/src/main/java/ravenexchange/backend/user/User.java
@@ -13,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
-    private long userID;
+    private long user_id;
 
     @Column(name = "username")
     private String username;
--- ravenexchange_backend/src/main/java/ravenexchange/backend/user/UserRepository.java
+++ ravenexchange_backend/src/main/java/ravenexchange/backend/user/UserRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
