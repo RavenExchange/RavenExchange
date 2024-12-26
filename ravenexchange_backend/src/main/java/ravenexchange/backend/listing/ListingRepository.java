7d8bd6098b9a3d04783f0f86e385c0112f79502e
Haashir Tanveer <haashirtanveer@gmail.com>
2024-12-26 16:48:53 -0500

    get listing by id implemented (/api/listing/get{id})

--- ravenexchange_backend/src/main/java/ravenexchange/backend/listing/ListingRepository.java
+++ ravenexchange_backend/src/main/java/ravenexchange/backend/listing/ListingRepository.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 @RepositoryRestResource
 public interface ListingRepository extends JpaRepository<Listing, Long> {
-    @Query("SELECT l FROM Listing l ORDER BY l.listing_id DESC")
+    @Query("SELECT l FROM Listing l ORDER BY l.listingId DESC")
     List<Listing> findTop10ByOrderByListingIdDesc(); // Fetch the top 10 entries based on listing_id in descending order
 
-    @Query("SELECT l FROM Listing l WHERE l.listing_category = :category ORDER BY l.listing_id DESC")
+    @Query("SELECT l FROM Listing l WHERE l.listingCategory = :category ORDER BY l.listingId DESC")
     List<Listing> findTop10ByListingCategoryOrderByListingIdDesc(@Param("category") String category); // Fetch the top 10 entries based on listing_id in descending order with a specific category
 
     // Find a listing by listing_id
